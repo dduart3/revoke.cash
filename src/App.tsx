58490373a9ac0dc3b461da0d7859b1f05f16e1a6
yashnaman <shahyashs145@gmail.com>
2021-03-03 22:22:14 +0530

    update source

--- src/App.tsx
+++ src/App.tsx
@@ -236,7 +236,7 @@ class App extends Component<{}, AppState> {
   renderFooter() {
     return (
       <div>
-        <p>Site created by <a href="https://kalis.me/">Rosco Kalis</a> (<a href="https://github.com/rkalis/revoke.cash">Source</a>)</p>
+        <p>Site created by <a href="https://kalis.me/">Rosco Kalis</a> (<a href="https://github.com/yashnaman/revoke.cash">Source</a>)</p>
         <p>Learn more: <a href="https://kalis.me/unlimited-erc20-allowances/">Unlimited ERC20 allowances considered harmful</a></p>
       </div>
     )
